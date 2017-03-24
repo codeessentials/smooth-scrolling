490118204fadfff449deede7065882f5f231c98d
Luan Ramos <luan@apiki.com>
2017-03-23 22:21:15 -0300

    scroll

---
+++ target.js
@@ -0,0 +1,13 @@
+jQuery('.navigation a').on( 'click', function(event) {
+	var target 	= jQuery( this );
+    var element = target.attr('href');
+
+    jQuery('.navigation a').removeClass('active')
+    target.addClass('active');
+
+    jQuery("body, html").animate({ 
+      scrollTop: jQuery( element ).offset().top 
+    }, 600);
+
+
+});
